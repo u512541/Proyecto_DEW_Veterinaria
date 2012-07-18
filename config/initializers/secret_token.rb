dc26210cb1d511721fa4509000c8554c3b8f17d4
u512541 <u512541@gmail.com>
2012-07-17 22:12:03 -0500

    first proyect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoDewVeterinaria::Application.config.secret_token = '52d8ab50bda906ec366b808470890ecb4a22e68ae63575985b52ca625972d8c2c73201ed075039bbcd89d264a1c5bd2792dc5c6fed185a106c073a8f6cb9f70b'
